c59c1dc57e9372915050643ac74ede5028d211de
Rohit Kandimalla <rohit.kandimalla@semanticbits.com>
2021-12-29 22:55:09 -0500

    Added madie-patient

--- src/madie-root-config.ts
+++ src/madie-root-config.ts
@@ -6,6 +6,7 @@ import authConfig from "./auth-config";
 import publicConfig from "./public-config";
 import componentsConfig from "./components-config";
 import measureConfig from "./measure-config";
+import patientConfig from "./patient-config";
 
 registerApplication<ApplicationProps>(layoutConfig);
 registerApplication<ApplicationProps>(editorConfig);
@@ -13,6 +14,7 @@ registerApplication<ApplicationProps>(authConfig);
 registerApplication<ApplicationProps>(publicConfig);
 registerApplication<ApplicationProps>(componentsConfig);
 registerApplication<ApplicationProps>(measureConfig);
+registerApplication<ApplicationProps>(patientConfig);
 
 start({
   urlRerouteOnly: true,
