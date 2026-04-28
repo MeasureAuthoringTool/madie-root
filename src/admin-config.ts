41aa240b1c976a20f346ef50b7dff13c0b5162c7
Ethan Kaplan <ethan.kaplan@semanticbits.com>
2026-04-28 11:30:08 -0700

    MAT-9639 admin setup

---
+++ src/admin-config.ts
@@ -0,0 +1,13 @@
+// import ApplicationConfig from "./ApplicationConfig";
+
+// const config: ApplicationConfig = {
+//   name: "@madie/madie-admin",
+//   app: () => System.import("@madie/madie-admin"),
+//   activeWhen: ["/admin"],
+//   customProps: {
+//     domElementGetter: (): HTMLElement | null =>
+//       document.getElementById("madie-admin"),
+//   },
+// };
+
+// export default config;
