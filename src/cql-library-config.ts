3aa670419d9181f6e9ab4a6a053d29617a90f895
mcmcphillips <matt.mcphillips@semanticBits.com>
2026-06-16 10:10:58 -0700

    MAT-8010: Revert config

--- src/cql-library-config.ts
+++ src/cql-library-config.ts
@@ -3,7 +3,7 @@ import ApplicationConfig from "./ApplicationConfig";
 const config: ApplicationConfig = {
   name: "@madie/madie-cql-library",
   app: () => System.import("@madie/madie-cql-library"),
-  activeWhen: ["/cql-libraries"],
+  activeWhen: ["/"],
   customProps: {
     domElementGetter: (): HTMLElement | null =>
       document.getElementById("madie-cql-library"),
